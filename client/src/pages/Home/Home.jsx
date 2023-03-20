0f0ebb19c3971312fc3c2e7df42a0350da8d52f4
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-03-20 10:15:21 +0000

    added background image to homepage

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,14 +2,18 @@ import Navbar from "./../../components/Navbar";
 import styles from "./home.module.scss";
 
 export default function Home() {
-  console.log(styles);
   return (
-    <div className={styles.pageBody}>
-      {/* <Navbar /> */}
-      <h1>Homes</h1>
-      <div className={styles.spiralDiv}>
-        <img src="images/spiral.jpg" className={styles.spiral} />
+    <>
+      <div className={styles.container}>
+        <div
+          className={styles.background}
+          style={{ backgroundImage: `url('/client/public/images/man-with-a-book.jpg'))` }}
+        />
+        <div className={styles.header}>
+          <h1>Hello, World!</h1>
+          <p>This is some text on top of the background image.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
