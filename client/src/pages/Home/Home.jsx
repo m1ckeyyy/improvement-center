8aa575a80150532d13d3d62e5c18669e428673e3
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-03-20 08:45:53 +0000

    added home button to login and register page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,14 +2,14 @@ import Navbar from "./../../components/Navbar";
 import styles from "./home.module.scss";
 
 export default function Home() {
-	console.log(styles);
-	return (
-		<div className={styles.pageBody}>
-			{/* <Navbar /> */}
-			<h1>Homes</h1>
-			<div className={styles.spiralDiv}>
-				<img src="images/spiral.jpg" className={styles.spiral} />
-			</div>
-		</div>
-	);
+  console.log(styles);
+  return (
+    <div className={styles.pageBody}>
+      {/* <Navbar /> */}
+      <h1>Homes</h1>
+      <div className={styles.spiralDiv}>
+        <img src="images/spiral.jpg" className={styles.spiral} />
+      </div>
+    </div>
+  );
 }
