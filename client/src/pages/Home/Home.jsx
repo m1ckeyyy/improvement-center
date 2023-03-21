b395d257cfd3ad24147f546b46c7c9d55dafb516
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-03-21 09:42:31 +0000

    navbar styling

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,17 +1,41 @@
 import Navbar from "./../../components/Navbar";
 import styles from "./home.module.scss";
+import { Link } from "react-router-dom";
+
+const handleLogoClick = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+};
 
 export default function Home() {
   return (
     <>
       <div className={styles.container}>
-        <div
-          className={styles.background}
-          style={{ backgroundImage: `url('/client/public/images/man-with-a-book.jpg'))` }}
-        />
         <div className={styles.header}>
-          <h1>Hello, World!</h1>
-          <p>This is some text on top of the background image.</p>
+          <nav className={styles.navbar}>
+            <img
+              src="/images/logo3.png"
+              alt="Logo"
+              className={styles.logo}
+              onClick={handleLogoClick}
+            />
+            <Link to="/tools" className={styles.navbarElement}>
+              Tools
+            </Link>
+            <Link to="/about" className={styles.navbarElement}>
+              About
+            </Link>
+            <Link to="/login" className={styles.navbarElement}>
+              Login
+            </Link>
+            <Link to="/register" className={styles.navbarElement}>
+              Register
+            </Link>
+            {/* <h1>Hello, World!</h1> */}
+            {/* <p>This is some text on top of the background image.</p> */}
+          </nav>
         </div>
       </div>
     </>
