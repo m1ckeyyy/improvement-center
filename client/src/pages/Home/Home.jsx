b087af6379dbc231562fb2badc6a1868c00eac0d
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-03-22 21:39:56 +0000

    test commit

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import Navbar from "./../../components/Navbar";
 import styles from "./home.module.scss";
+import { AiOutlineHome, AiOutlineUser, AiOutlineMenu } from "react-icons/ai";
+
 import { Link } from "react-router-dom";
 
 const handleLogoClick = () => {
@@ -13,31 +15,69 @@ export default function Home() {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <img
+        <header className={styles.header}>
+          <Link to="/" className={styles.logo}>
+            <AiOutlineHome
+              color="greenyellow"
+              size="28px"
+              style={{ marginRight: "3px" }}
+            />
+            <span>IC</span>
+          </Link>
+          <ul className={styles.navbar}>
+            <li>
+              <Link
+                to="/"
+                className={`${styles.navbarElement} ${styles.active}`}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className={styles.navbarElement}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="/tools" className={styles.navbarElement}>
+                Tools
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" className={styles.navbarElement}>
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register" className={styles.navbarElement}>
+                Register
+              </Link>
+            </li>
+          </ul>
+          <div className={styles.main}>
+            <Link to="#" className={styles.user}>
+              <AiOutlineUser
+                color="greenyellow"
+                size="28px"
+                style={{ marginRight: "7px" }}
+              />
+              Login
+            </Link>
+            <Link to="#">Register</Link>
+            <div>
+              <AiOutlineMenu className={styles.menuIcon} />
+            </div>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
+{
+  /* <img
             src="/images/logo3.png"
             alt="Logo"
             className={styles.logo}
             onClick={handleLogoClick}
-          />
-          <nav className={styles.navbar}>
-            <Link to="/tools" className={styles.navbarElement}>
-              Tools
-            </Link>
-            <Link to="/about" className={styles.navbarElement}>
-              About
-            </Link>
-            <Link to="/login" className={styles.navbarElement}>
-              Login
-            </Link>
-            <Link to="/register" className={styles.navbarElement}>
-              Register
-            </Link>
-            {/* <h1>Hello, World!</h1> */}
-            {/* <p>This is some text on top of the background image.</p> */}
-          </nav>
-        </div>
-      </div>
-    </>
-  );
+          /> */
 }
