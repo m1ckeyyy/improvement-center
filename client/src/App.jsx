1ad8779d7d79f7840b8261b703e4cd508d5410b9
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-03-25 14:15:41 +0000

    added SuspenseRouter to keep state of the page until the clicked route fully loads

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useTransition } from "react";
 
 const About = lazy(() => import("./pages/About"));
 const Login = lazy(() => import("./pages/Login/Login"));
@@ -9,22 +9,27 @@ const Tools = lazy(() => import("./pages/Tools/Tools"));
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import SuspenseRouter from "./SuspenseRouter";
+
 // on launch hacker animation every 7s
+
 function App() {
-	return (
-		<>
-			{/* <Navbar /> */}
-			<Suspense fallback={<h1>wait</h1>}>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />
-					<Route path="/login" element={<Login />} />
-					<Route path="/register" element={<Register />} />
-					<Route path="/tools" element={<Tools />} />
-				</Routes>
-			</Suspense>
-		</>
-	);
+  //   const [isPending, startTransition] = useTransition();
+  return (
+    <>
+      <SuspenseRouter window={window}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/tools" element={<Tools />} />
+          </Routes>
+        </Suspense>
+      </SuspenseRouter>
+    </>
+  );
 }
 
 export default App;
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,11 @@
 export default function About() {
-	return <h1>About</h1>;
-	//
+  return <h1>About</h1>;
+  //
 }
+
 /*
+	- apply useTransition in App.jsx to disable loading screen on route change
+	- change color, height, positioning on homepage dropdown menu 
 	- show password
 	- caps lock reminder
 	- homepage styling
