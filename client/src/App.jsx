04728f9a3f196257c9a60a4f26de10706a014969
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2023-03-26 22:49:24 +0200

    moved home to navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ const Login = lazy(() => import("./pages/Login/Login"));
 const Register = lazy(() => import("./pages/Register/Register"));
 const Home = lazy(() => import("./pages/Home/Home"));
 const Tools = lazy(() => import("./pages/Tools/Tools"));
+const Navbar = lazy(() => import("./pages/Navbar/Navbar"));
 
 import { Route, Routes } from "react-router-dom";
 
@@ -15,22 +16,22 @@ import SuspenseRouter from "./SuspenseRouter";
 // on launch hacker animation every 7s
 
 function App() {
-  //   const [isPending, startTransition] = useTransition();
-  return (
-    <>
-      <SuspenseRouter window={window}>
-        <Suspense fallback={<LoadingScreen />}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/tools" element={<Tools />} />
-          </Routes>
-        </Suspense>
-      </SuspenseRouter>
-    </>
-  );
+	//   const [isPending, startTransition] = useTransition();
+	return (
+		<>
+			<SuspenseRouter window={window}>
+				<Suspense fallback={<LoadingScreen />}>
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/about" element={<About />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/tools" element={<Tools />} />
+					</Routes>
+				</Suspense>
+			</SuspenseRouter>
+		</>
+	);
 }
 
 export default App;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,118 +1,13 @@
-// import Navbar from "./../../components/Navbar";
+import React from "react";
+import Navbar from "./../Navbar/Navbar";
 import styles from "./home.module.scss";
-import {
-  AiOutlineHome,
-  AiOutlineUser,
-  AiOutlineMenu,
-  AiOutlineClose,
-} from "react-icons/ai";
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
 
-const handleLogoClick = () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+const Home = () => {
+	return (
+		<div className={styles.container}>
+			<Navbar />
+		</div>
+	);
 };
 
-export default function Home() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  return (
-    <>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <Link to="/" className={styles.logo} onClick={handleLogoClick}>
-            <AiOutlineHome
-              color="#b4e4ff"
-              size="28px"
-              style={{
-                marginRight: "4px",
-                marginBottom: "4px",
-                minWidth: "28px",
-                minHeight: "28px",
-              }}
-            />
-            <span className={styles.title}>Improvement Center</span>
-          </Link>
-          <ul
-            className={`${styles.navbar} ${isMenuOpen ? styles.menuOpen : ""}`}
-          >
-            <li>
-              <Link
-                to="/"
-                className={`${styles.navbarElement} ${styles.active}`}
-              >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/about" className={styles.navbarElement}>
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="/tools" className={styles.navbarElement}>
-                Tools
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/login"
-                className={`${styles.navbarElement} ${styles.phoneMedia}`}
-              >
-                Login
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/register"
-                className={`${styles.navbarElement} ${styles.phoneMedia}`}
-              >
-                Register
-              </Link>
-            </li>
-          </ul>
-          <div className={styles.main}>
-            <Link to="/login" className={styles.user}>
-              <AiOutlineUser
-                color="#b4e4ff"
-                size="28px"
-                style={{ marginRight: "7px" }}
-              />
-              <span>Login</span>
-            </Link>
-            <Link to="/register">
-              <span>Register</span>
-            </Link>
-            <div>
-              {isMenuOpen ? (
-                <AiOutlineClose
-                  className={styles.menuIcon}
-                  onClick={toggleMenu}
-                />
-              ) : (
-                <AiOutlineMenu
-                  className={styles.menuIcon}
-                  onClick={toggleMenu}
-                />
-              )}
-            </div>
-          </div>
-        </header>
-      </div>
-    </>
-  );
-}
-{
-  /* <img
-            src="/images/logo3.png"
-            alt="Logo"
-            className={styles.logo}
-            onClick={handleLogoClick}
-          /> */
-}
+export default Home;
