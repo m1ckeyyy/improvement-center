fde2a5b677829ba3330b22c18a3410f3b8bf08dd
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-03-31 14:27:58 +0000

    homepage added gridlayout

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,39 +6,48 @@ import { Link } from "react-router-dom";
 import { useHeaderEffect } from "./../effects/hackerEffect";
 
 const Home = () => {
-	const ultimate = useRef(null);
-	useEffect(() => {
-		// useHeaderEffect(ultimate.current);
-	}, []);
+  const ultimate = useRef(null);
+  useEffect(() => {
+    // useHeaderEffect(ultimate.current);
+  }, []);
 
-	return (
-		<div className={styles.container}>
-			<Navbar />
-			<section className={styles.mainSection}>
-				<div className={styles.mainContent}>
-					<h1 className={styles.mainHeading}>
-						The{" "}
-						<span ref={ultimate} data-value="ULTIMATE">
-							Ultimate
-						</span>{" "}
-						<br />
-						Productivity Hub
-					</h1>
-					<p className={styles.mainDescription}>
-						"The secret of getting ahead is getting started." <br />- Mark Twain
-					</p>
-					<img src="images/wmn3.png" alt="woman" className={styles.womanImg} />
+  return (
+    <div className={styles.container}>
+      <Navbar />
+      <section className={styles.mainSection}>
+        <div className={styles.mainContent}>
+          <div className={styles.leftContent}>
+            <h1 className={styles.mainHeading}>
+              The{" "}
+              <span ref={ultimate} data-value="ULTIMATE">
+                Ultimate
+              </span>{" "}
+              <br />
+              Productivity Hub
+            </h1>
+            <p className={styles.mainDescription}>
+              "The secret of getting ahead is getting started." <br />- Mark
+              Twain
+            </p>
+            <div className={styles.btnContainer}>
+              <Link to="/tools" className={styles.btnGetStarted}>
+                So Get Started
+              </Link>
+            </div>
+          </div>
+          <div className={styles.rightContent}>
+            <img
+              src="images/wmn3.png"
+              alt="woman"
+              className={styles.womanImg}
+            />
+          </div>
 
-					{/* change this to scroll to tools seciton👇 */}
-					<div className={styles.btnContainer}>
-						<Link to="/tools" className={styles.btnGetStarted}>
-							So Get Started
-						</Link>
-					</div>
-				</div>
-			</section>
-		</div>
-	);
+          {/* change this to scroll to tools seciton👇 */}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
