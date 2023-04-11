d523b5c3c174eacb6dd2b0ae5350c595d50af5b3
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-04-11 11:52:40 +0000

    scroll icon transition

--- client/src/components/LandingSection/LandingSection.jsx
+++ client/src/components/LandingSection/LandingSection.jsx
@@ -6,53 +6,48 @@ import { Link } from "react-router-dom";
 import { useHeaderEffect } from "./../../pages/effects/hackerEffect";
 
 const LandingPage = () => {
-	const ultimate = useRef(null);
-	useEffect(() => {
-		// useHeaderEffect(ultimate.current);
-	}, []);
-	const handleScrollClick = () => {
-		window.scrollTo({
-			top: window.pageYOffset + window.innerHeight,
-			behavior: "smooth",
-		});
-	};
+  const ultimate = useRef(null);
+  useEffect(() => {
+    // useHeaderEffect(ultimate.current);
+  }, []);
+  const handleScrollClick = () => {
+    window.scrollTo({
+      top: window.pageYOffset + window.innerHeight,
+      behavior: "smooth",
+    });
+  };
 
-	return (
-		<section className={styles.mainSection}>
-			<div className={styles.mainContent}>
-				<div className={styles.leftContent}>
-					<h1 className={styles.mainHeading}>
-						The{" "}
-						<span ref={ultimate} data-value="ULTIMATE">
-							Ultimate
-						</span>{" "}
-						<br />
-						Productivity Hub
-					</h1>
-					<p className={styles.mainDescription}>
-						"The secret of getting ahead is getting started." <br />- Mark Twain
-					</p>
-					<div className={styles.btnContainer}>
-						<div
-							to="/"
-							className={styles.btnGetStarted}
-							onClick={handleScrollClick}
-						>
-							So Get Started
-						</div>
-					</div>
-					<BsMouse
-						color="white"
-						className={styles.scrollIcon}
-						onClick={handleScrollClick}
-					/>
-				</div>
-				<div className={styles.rightContent}>
-					<img src="images/wmn3.png" alt="woman" className={styles.womanImg} />
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <section className={styles.mainSection}>
+      <div className={styles.mainContent}>
+        <div className={styles.leftContent}>
+          <h1 className={styles.mainHeading}>
+            The{" "}
+            <span ref={ultimate} data-value="ULTIMATE">
+              Ultimate
+            </span>{" "}
+            <br />
+            Productivity Hub
+          </h1>
+          <p className={styles.mainDescription}>
+            "The secret of getting ahead is getting started." <br />- Mark Twain
+          </p>
+          <div className={styles.btnContainer}>
+            <div
+              to="/"
+              className={styles.btnGetStarted}
+              onClick={handleScrollClick}
+            >
+              So Get Started
+            </div>
+          </div>
+        </div>
+        <div className={styles.rightContent}>
+          <img src="images/wmn3.png" alt="woman" className={styles.womanImg} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default LandingPage;
--- client/src/components/ToolsSection/ToolsSection.jsx
+++ client/src/components/ToolsSection/ToolsSection.jsx
@@ -3,7 +3,7 @@ import styles from "./ToolsSection.module.scss";
 const ToolsSection = () => {
   return (
     <>
-      <div className={styles.container}>siema</div>
+      <div className={styles.container} id='tools'>siema</div>
     </>
   );
 };
