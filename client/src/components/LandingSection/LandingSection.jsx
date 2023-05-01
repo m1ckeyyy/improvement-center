83f1e1213168d3abb4fff78cc32eda7aeeb66d41
m1ckeyyy <mikolaj.krawczynski3@gmail.com>
2023-05-01 15:28:22 +0200

    added smooth scrolling to scrollIcon

--- client/src/components/LandingSection/LandingSection.jsx
+++ client/src/components/LandingSection/LandingSection.jsx
@@ -7,49 +7,49 @@ import styles from "./landingPage.module.scss";
 // import { useHeaderEffect } from "./../../pages/effects/hackerEffect";
 
 const LandingPage = () => {
-  // const ultimate = useRef(null);
-  // useEffect(() => {
-  // useHeaderEffect(ultimate.current);
-  // }, []);
-  const handleScrollClick = () => {
-    window.scrollTo({
-      top: window.pageYOffset + window.innerHeight,
-      behavior: "smooth",
-    });
-  };
+	// const ultimate = useRef(null);
+	// useEffect(() => {
+	// useHeaderEffect(ultimate.current);
+	// }, []);
+	const handleScrollClick = () => {
+		window.scrollTo({
+			top: window.pageYOffset + window.innerHeight,
+			behavior: "smooth",
+		});
+	};
 
-  return (
-    <section className={styles.mainSection}>
-      <div className={styles.mainContent}>
-        <div className={styles.leftContent}>
-          <h1 className={styles.mainHeading}>
-            The {/* <span ref={ultimate} data-value="ULTIMATE"> */}
-            Ultimate
-            {/* </span>{" "} */}
-            <br />
-            Productivity Hub
-          </h1>
-          <p className={styles.mainDescription}>
-            "The secret of getting ahead is getting started." <br />- Mark Twain
-          </p>
-          <div className={styles.btnContainer}>
-            <Link
-              smooth={true}
-              offset={20}
-              duration={500}
-              to="tools"
-              className={styles.btnGetStarted}
-            >
-              So Get Started
-            </Link>
-          </div>
-        </div>
-        <div className={styles.rightContent}>
-          <img src="images/wmn3.png" alt="woman" className={styles.womanImg} />
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.mainSection}>
+			<div className={styles.mainContent}>
+				<div className={styles.leftContent}>
+					<h1 className={styles.mainHeading}>
+						The {/* <span ref={ultimate} data-value="ULTIMATE"> */}
+						Ultimate
+						{/* </span>{" "} */}
+						<br />
+						Productivity Hub
+					</h1>
+					<p className={styles.mainDescription}>
+						"The secret of getting ahead is getting started." <br />- Mark Twain
+					</p>
+					<div className={styles.btnContainer}>
+						<Link
+							smooth={true}
+							offset={0}
+							duration={500}
+							to="tools"
+							className={styles.btnGetStarted}
+						>
+							So Get Started
+						</Link>
+					</div>
+				</div>
+				<div className={styles.rightContent}>
+					<img src="images/wmn3.png" alt="woman" className={styles.womanImg} />
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default LandingPage;
--- client/src/pages/Navbar/Navbar.jsx
+++ client/src/pages/Navbar/Navbar.jsx
@@ -3,188 +3,188 @@ import { Link } from "react-scroll";
 import { animateScroll as scroll } from "react-scroll";
 import styles from "./navbar.module.scss";
 import {
-  AiOutlineHome,
-  AiOutlineUser,
-  AiOutlineMenu,
-  AiOutlineClose,
+	AiOutlineHome,
+	AiOutlineUser,
+	AiOutlineMenu,
+	AiOutlineClose,
 } from "react-icons/ai";
 import { BsMouse } from "react-icons/bs";
 import React, { useState, useEffect } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 const handleLogoClick = () => {
-  scroll.scrollTo(0, {
-    duration: 1000,
-    smooth: "easeInOutQuint",
-  });
+	scroll.scrollTo(0, {
+		duration: 1000,
+		smooth: "easeInOutQuint",
+	});
 };
 
 export default function Navbar() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [showNav, setShowNav] = useState(true);
-  const [prevScrollY, setPrevScrollY] = useState(0);
-  const [navBg, setNavBg] = useState(false);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [showNav, setShowNav] = useState(true);
+	const [prevScrollY, setPrevScrollY] = useState(0);
+	const [navBg, setNavBg] = useState(false);
 
-  const controlNav = () => {
-    const currentScrollY = window.scrollY;
-    // console.log(`current: ${currentScrollY} prev: ${prevScrollY}`);
-    if (showNav && window.scrollY > 150) {
-      setNavBg(true);
-    } else {
-      setNavBg(false);
-    }
-    if (currentScrollY > prevScrollY) {
-      setShowNav(false);
-      setPrevScrollY(currentScrollY);
-    } else {
-      setShowNav(true);
-      setPrevScrollY(currentScrollY);
-      return;
-    }
+	const controlNav = () => {
+		const currentScrollY = window.scrollY;
+		// console.log(`current: ${currentScrollY} prev: ${prevScrollY}`);
+		if (showNav && window.scrollY > 150) {
+			setNavBg(true);
+		} else {
+			setNavBg(false);
+		}
+		if (currentScrollY > prevScrollY) {
+			setShowNav(false);
+			setPrevScrollY(currentScrollY);
+		} else {
+			setShowNav(true);
+			setPrevScrollY(currentScrollY);
+			return;
+		}
 
-    // console.log(showNav);
-    if (window.scrollY > 100) {
-      setShowNav(false);
-    } else {
-      setShowNav(true);
-    }
-  };
+		// console.log(showNav);
+		if (window.scrollY > 100) {
+			setShowNav(false);
+		} else {
+			setShowNav(true);
+		}
+	};
 
-  useEffect(() => {
-    window.addEventListener("scroll", controlNav);
-    return () => {
-      window.removeEventListener("scroll", controlNav);
-    };
-  });
+	useEffect(() => {
+		window.addEventListener("scroll", controlNav);
+		return () => {
+			window.removeEventListener("scroll", controlNav);
+		};
+	});
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+	};
 
-  return (
-    <>
-      <div className={styles.navContainer}>
-        <header
-          className={`${styles.header} ${showNav ? "" : styles.hideNav} ${
-            navBg ? styles.navBackground : ""
-          }`}
-        >
-          <a href="#" className={styles.logo} onClick={handleLogoClick}>
-            <AiOutlineHome
-              className={styles.animatedLogoIcon}
-              color="#b4e4ff"
-              size="28px"
-              style={{
-                marginRight: "4px",
-                marginBottom: "4px",
-                marginTop: "4px",
-                minWidth: "28px",
-                minHeight: "28px",
-              }}
-            />
-            <span className={styles.title}>Improvement Center</span>
-          </a>
-          <ul
-            className={`${styles.navbar} ${isMenuOpen ? styles.menuOpen : ""}`}
-          >
-            <li>
-              <a
-                onClick={handleLogoClick}
-                href="#"
-                className={`${styles.navbarElement} ${styles.active}`}
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <Link
-                to="tools"
-                smooth={true}
-                offset={20}
-                duration={500}
-                className={styles.navbarElement}
-              >
-                Tools
-              </Link>
-              {/* <a
+	return (
+		<>
+			<div className={styles.navContainer}>
+				<header
+					className={`${styles.header} ${showNav ? "" : styles.hideNav} ${
+						navBg ? styles.navBackground : ""
+					}`}
+				>
+					<a href="#" className={styles.logo} onClick={handleLogoClick}>
+						<AiOutlineHome
+							className={styles.animatedLogoIcon}
+							color="#b4e4ff"
+							size="28px"
+							style={{
+								marginRight: "4px",
+								marginBottom: "4px",
+								marginTop: "4px",
+								minWidth: "28px",
+								minHeight: "28px",
+							}}
+						/>
+						<span className={styles.title}>Improvement Center</span>
+					</a>
+					<ul
+						className={`${styles.navbar} ${isMenuOpen ? styles.menuOpen : ""}`}
+					>
+						<li>
+							<a
+								onClick={handleLogoClick}
+								href="#"
+								className={`${styles.navbarElement} ${styles.active}`}
+							>
+								Home
+							</a>
+						</li>
+						<li>
+							<Link
+								to="tools"
+								smooth={true}
+								offset={20}
+								duration={500}
+								className={styles.navbarElement}
+							>
+								Tools
+							</Link>
+							{/* <a
                 // onClick={scrollToAbout}
                 href="#about"
                 className={styles.navbarElement}
               >
                 About
               </a> */}
-            </li>
-            <li>
-              <Link
-                to="about"
-                smooth={true}
-                offset={-30}
-                duration={500}
-                className={styles.navbarElement}
-              >
-                About
-              </Link>
+						</li>
+						<li>
+							<Link
+								to="about"
+								smooth={true}
+								offset={-30}
+								duration={500}
+								className={styles.navbarElement}
+							>
+								About
+							</Link>
 
-              {/* <a href="#tools" className={styles.navbarElement}>
+							{/* <a href="#tools" className={styles.navbarElement}>
                 Tools
               </a> */}
-            </li>
-            <li>
-              <RouterLink
-                to="/login"
-                className={`${styles.navbarElement} ${styles.phoneMedia}`}
-              >
-                <AiOutlineUser style={{ marginRight: "3px" }} />
-                Login
-              </RouterLink>
-            </li>
-            <li>
-              <RouterLink
-                to="/register"
-                className={`${styles.navbarElement} ${styles.phoneMedia}`}
-              >
-                <AiOutlineUser style={{ marginRight: "3px" }} />
-                Register
-              </RouterLink>
-            </li>
-          </ul>
-          <div className={styles.main}>
-            <RouterLink to="/login" className={styles.user}>
-              <AiOutlineUser
-                color="#b4e4ff"
-                size="28px"
-                style={{ marginRight: "7px" }}
-              />
-              <span>Login</span>
-            </RouterLink>
-            <RouterLink to="/register">
-              <span>Register</span>
-            </RouterLink>
-            <div tabIndex="0">
-              {isMenuOpen ? (
-                <AiOutlineClose
-                  className={styles.menuIcon}
-                  onClick={toggleMenu}
-                />
-              ) : (
-                <AiOutlineMenu
-                  className={styles.menuIcon}
-                  onClick={toggleMenu}
-                />
-              )}
-            </div>
-          </div>
-        </header>
-        <div className={styles.iconContainer}>
-          <a href="#tools">
-            <BsMouse
-              color="white"
-              className={styles.scrollIcon}
-              // onClick={handleScrollClick}
-            />
-          </a>
-        </div>
-      </div>
-    </>
-  );
+						</li>
+						<li>
+							<RouterLink
+								to="/login"
+								className={`${styles.navbarElement} ${styles.phoneMedia}`}
+							>
+								<AiOutlineUser style={{ marginRight: "3px" }} />
+								Login
+							</RouterLink>
+						</li>
+						<li>
+							<RouterLink
+								to="/register"
+								className={`${styles.navbarElement} ${styles.phoneMedia}`}
+							>
+								<AiOutlineUser style={{ marginRight: "3px" }} />
+								Register
+							</RouterLink>
+						</li>
+					</ul>
+					<div className={styles.main}>
+						<RouterLink to="/login" className={styles.user}>
+							<AiOutlineUser
+								color="#b4e4ff"
+								size="28px"
+								style={{ marginRight: "7px" }}
+							/>
+							<span>Login</span>
+						</RouterLink>
+						<RouterLink to="/register">
+							<span>Register</span>
+						</RouterLink>
+						<div tabIndex="0">
+							{isMenuOpen ? (
+								<AiOutlineClose
+									className={styles.menuIcon}
+									onClick={toggleMenu}
+								/>
+							) : (
+								<AiOutlineMenu
+									className={styles.menuIcon}
+									onClick={toggleMenu}
+								/>
+							)}
+						</div>
+					</div>
+				</header>
+				<div className={styles.iconContainer}>
+					<Link smooth={true} offset={0} duration={500} to="tools">
+						<BsMouse
+							color="white"
+							className={styles.scrollIcon}
+							// onClick={handleScrollClick}
+						/>
+					</Link>
+				</div>
+			</div>
+		</>
+	);
 }
