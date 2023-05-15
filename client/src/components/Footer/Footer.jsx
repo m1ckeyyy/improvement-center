0eda5d1f263e602f77349a52b41fb5949fc972d8
m1ckeyyy <90149242+m1ckeyyy@users.noreply.github.com>
2023-05-15 14:19:25 +0000

    added flexbox to footer and all the elements

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,15 +1,72 @@
 import styles from "./Footer.module.scss";
+import { Link } from "react-scroll";
+import { animateScroll as scroll } from "react-scroll";
+
+const handleHomeClick = () => {
+  scroll.scrollTo(0, {
+    duration: 1000,
+    smooth: "easeInOutQuint",
+  });
+};
 
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <h1>Foooooooooooooter</h1>
-      <form>
-        <label>
-          Found a Bug? 🪲 Send Feedback! <input type="text" name="email" />
-        </label>
-        <input type="submit" value="submit" />
-      </form>
+      <div className={styles.sectionOne}>
+        <h2>IMPROVEMENT CENTER</h2>
+        <h3>The Ultimate Productivity Hub</h3>
+      </div>
+
+      <div className={styles.sectionTwo}>
+        <h2>Navigation</h2>
+        <a href="#" onClick={handleHomeClick}>
+          <h3> Home</h3>
+        </a>
+        <br />
+        <Link
+          to="tools"
+          smooth={true}
+          offset={20}
+          duration={500}
+          className={styles.footerLink}
+        >
+          <h3>Tools</h3>
+        </Link>
+        <br />
+        <Link
+          to="about"
+          smooth={true}
+          offset={-20}
+          duration={500}
+          className={styles.footerLink}
+        >
+          <h3>About</h3>
+        </Link>{" "}
+      </div>
+      <div className={styles.sectionThree}>
+        <h2>Useful Links</h2>
+        <a href="https://github.com/m1ckeyyy/login-app-frontend">
+          <h3>GitHub Repository</h3>
+        </a>
+        <br />
+        <a href="https://github.com/m1ckeyyy">
+          <h3>GitHub Profile</h3>
+        </a>
+        <br />
+        <a href="https://www.linkedin.com/in/mikolaj-krawczynski-a82ba5240/">
+          <h3>LinkedIn</h3>
+        </a>
+      </div>
+      <div className={styles.sectionFour}>
+        {" "}
+        <h2>Found a Bug? 🪲</h2>
+        Send Feedback! Drop your e-mail
+        <br />
+        <div className={styles.form}>
+          <input type="text" name="email" className={styles.formInput} />
+          <input type="submit" value="⬅" className={styles.submitInput} />
+        </div>
+      </div>
     </div>
   );
 };
