f06c5e413ffe903190396368d5fa8643ad57d2a1
m1ckeyyy <90149242+m1ckeyyy@users.noreply.github.com>
2023-06-01 12:52:40 +0000

    first steps for pomodorotimer

--- client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
@@ -1,10 +1,20 @@
 import styles from "./PomodoroTimer.module.scss";
 
-function Tools() {
+function PomodoroTimer() {
   return (
     <div className={styles.container}>
-      <div className={styles.app}>{/* Rest of the timer component */}</div>
+      <div className={styles.app}>
+        <img
+          src="images/pomodoro2icon.png"
+          alt="pomodoro"
+          className={styles.pomodoroImg}
+        />
+        <div className={styles.timer}>
+          <span className={styles.workTimer}>25 : 00</span>
+          <span className={styles.breakTimer}>05 : 00</span>
+        </div>
+      </div>
     </div>
   );
 }
-export default Tools;
+export default PomodoroTimer;
