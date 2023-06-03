5add5170d4c2e0fa5bb96883a7b8b8ddd47ecdfc
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2023-06-03 22:55:23 +0200

    changed timerState in pomodoroTimer

--- client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
@@ -1,5 +1,5 @@
 import styles from "./PomodoroTimer.module.scss";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // const getState = () => {
 //   const timePassed = JSON.parse(localStorage.getItem('timePassed')) || 0;
@@ -8,53 +8,63 @@ import { useState, useEffect } from 'react';
 //   return { timePassed, workMode, userSettings };
 // };
 
-  
 function PomodoroTimer() {
-  // const [timerState, setTimerState] = useState(getState());
-    const [timePassed, setTimePassed] = useState(JSON.parse(localStorage.getItem('timePassed')) || 0);
-  const [workMode, setWorkMode] = useState(JSON.parse(localStorage.getItem('workMode')) || true);
-  const [workTime, setWorkTime] = useState(JSON.parse(localStorage.getItem('workTime')) || 25);
-  const [breakTime, setBreakTime] = useState(JSON.parse(localStorage.getItem('breakTime')) || 5);
-  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || 'default');
-  const [music, setMusic] = useState(JSON.parse(localStorage.getItem('music')) || 'on');
+	// const [timerState, setTimerState] = useState(getState());
+	const [timePassed, setTimePassed] = useState(
+		JSON.parse(localStorage.getItem("timePassed")) || 0
+	);
+	const [workMode, setWorkMode] = useState(
+		JSON.parse(localStorage.getItem("workMode")) || true
+	);
+	const [workTime, setWorkTime] = useState(
+		JSON.parse(localStorage.getItem("workTime")) || 25
+	);
+	const [breakTime, setBreakTime] = useState(
+		JSON.parse(localStorage.getItem("breakTime")) || 5
+	);
+	const [theme, setTheme] = useState(
+		JSON.parse(localStorage.getItem("theme")) || "default"
+	);
+	const [music, setMusic] = useState(
+		JSON.parse(localStorage.getItem("music")) || "on"
+	);
 
-  console.log(timerState)
-  const saveTimerState = (state) => {
-    localStorage.setItem('timerState', JSON.stringify(state));
-  };
+	// console.log(timerState)
+	console.log(timePassed, workMode, workTime, breakTime, theme, music);
+	const saveTimerState = (state) => {
+		localStorage.setItem("timerState", JSON.stringify(state));
+	};
 
-  const clearTimerState = () => {
-    localStorage.removeItem('timerState');
-  };
+	const clearTimerState = () => {
+		localStorage.removeItem("timerState");
+	};
 
-  const handlePageRefresh = () => {
-    saveTimerState(timerState); //zapisz wszystkie settingsy
-  };
+	const handlePageRefresh = () => {
+		saveTimerState(timePassed, workMode, workTime, breakTime, theme, music); //zapisz wszystkie settingsy
+	};
 
-  useEffect(() => {
-    window.addEventListener('beforeunload', handlePageRefresh);
+	useEffect(() => {
+		window.addEventListener("beforeunload", handlePageRefresh);
 
-    return () => {
-      window.removeEventListener('beforeunload', handlePageRefresh);
-    };
-  }, [timerState]); //dependecies = wszystkie settingsy
+		return () => {
+			window.removeEventListener("beforeunload", handlePageRefresh);
+		};
+	}, [timePassed, workMode, workTime, breakTime, theme, music]); //dependecies = wszystkie settingsy
 
-
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.app}>
-        <img
-          src="images/pomodoro2icon.png"
-          alt="pomodoro"
-          className={styles.pomodoroImg}
-        />
-        <div className={styles.timer}>
-          <span className={styles.workTimer}>00 : 00</span>
-          {/* <span className={styles.breakTimer}>05 : 00</span> */}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.app}>
+				<img
+					src="images/pomodoro2icon.png"
+					alt="pomodoro"
+					className={styles.pomodoroImg}
+				/>
+				<div className={styles.timer}>
+					<span className={styles.workTimer}>00 : 00</span>
+					{/* <span className={styles.breakTimer}>05 : 00</span> */}
+				</div>
+			</div>
+		</div>
+	);
 }
 export default PomodoroTimer;
