2b1121fea2373c2a4ee1d83f81e1227e9d2b7e61
m1ckeyyy <90149242+m1ckeyyy@users.noreply.github.com>
2023-06-11 12:58:03 +0000

    added circularProgressbar and styling

--- client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
@@ -1,9 +1,13 @@
 import styles from "./PomodoroTimer.module.scss";
 import { useState, useEffect } from "react";
 import { usePomodoroTimer } from "./PomodoroFunctions";
-import { ProgressBar } from "./ProgressBar.jsx";
+// import { ProgressBar } from "./ProgressBar.jsx";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function PomodoroTimer() {
+  const [percentage, setPercentage] = useState(1);
+  const timeInMinutes = false;
   const {
     timePassed,
     setTimePassed,
@@ -19,16 +23,18 @@ function PomodoroTimer() {
     setMusic,
   } = usePomodoroTimer();
 
-  const [progressValue, setProgressValue] = useState(0);
+  //   const [progressValue, setProgressValue] = useState(0);
+
   useEffect(() => {
     const interval = setInterval(() => {
-      setProgressValue((prev) => prev + 1);
+      setPercentage((prev) => prev + 1);
     }, 1000);
 
     return () => {
       clearInterval(interval);
     };
   });
+
   return (
     <div className={styles.container}>
       <div className={styles.app}>
@@ -38,14 +44,30 @@ function PomodoroTimer() {
           className={styles.pomodoroImg}
         />
         <div className={styles.timer}>
-          <span className={styles.workTimer}>20:00</span>
-          <div className={styles.items}>
-            <ProgressBar progress={progressValue} />
-            {/* <div class="canvas-wrap">
-              <canvas id="canvas" width="300" height="300"></canvas>
-              <span id="procent"></span>
-            </div> */}
-          </div>
+          <CircularProgressbar
+            value={percentage}
+            text={timeInMinutes ? `${timePassed}` : `${percentage}%`}
+            strokeWidth={6}
+            circleRatio={0.75}
+            className={styles.progress}
+            // background
+            // backgroundPadding={6}
+            styles={buildStyles({
+              pathTransitionDuration: 0.5,
+              strokeLinecap: "butt",
+              rotation: 1 / 2 + 1 / 8,
+              pathColor: `rgba(${(63 / percentage) * 100}, ${
+                percentage < 15 ? 160 : 199
+              }, 44)`,
+              textColor: `rgba(${(63 / percentage) * 100}, ${
+                percentage < 15 ? 160 : 199
+              }, 44)`,
+              trailColor: "lightgray",
+              fontSize: "10px",
+              //   backgroundColor: "#061A94",
+            })}
+          />
+          {/* <span className={styles.workTimer}>20:00</span> */}
         </div>
       </div>
     </div>
