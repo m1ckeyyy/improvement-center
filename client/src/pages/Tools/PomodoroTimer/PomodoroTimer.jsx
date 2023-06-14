ccb31f374857efee12cdbceeb0dd2783a462434f
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2023-06-14 23:31:02 +0200

    added timer function buttons and positioned

--- client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroTimer.jsx
@@ -1,76 +1,85 @@
 import styles from "./PomodoroTimer.module.scss";
 import { useState, useEffect } from "react";
 import { usePomodoroTimer } from "./PomodoroFunctions";
+import { AiOutlinePlayCircle, AiOutlinePauseCircle } from "react-icons/ai";
+import { RxReset } from "react-icons/rx";
+import { BsSkipForward } from "react-icons/bs";
 // import { ProgressBar } from "./ProgressBar.jsx";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
 function PomodoroTimer() {
-  const [percentage, setPercentage] = useState(1);
-  const timeInMinutes = false;
-  const {
-    timePassed,
-    setTimePassed,
-    workMode,
-    setWorkMode,
-    workTime,
-    setWorkTime,
-    breakTime,
-    setBreakTime,
-    theme,
-    setTheme,
-    music,
-    setMusic,
-  } = usePomodoroTimer();
+	const [percentage, setPercentage] = useState(1);
+	const timeInMinutes = false;
+	const {
+		timePassed,
+		setTimePassed,
+		workMode,
+		setWorkMode,
+		workTime,
+		setWorkTime,
+		breakTime,
+		setBreakTime,
+		theme,
+		setTheme,
+		music,
+		setMusic,
+	} = usePomodoroTimer();
 
-  //   const [progressValue, setProgressValue] = useState(0);
+	//   const [progressValue, setProgressValue] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPercentage((prev) => prev + 1);
-    }, 1000);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setPercentage((prev) => prev + 1);
+		}, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  });
+		return () => {
+			clearInterval(interval);
+		};
+	});
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.app}>
-        <img
-          src="images/pomodoro2icon.png"
-          alt="pomodoro"
-          className={styles.pomodoroImg}
-        />
-        <div className={styles.timer}>
-          <CircularProgressbar
-            value={percentage}
-            text={timeInMinutes ? `${timePassed}` : `${percentage}%`}
-            strokeWidth={6}
-            circleRatio={0.75}
-            className={styles.progress}
-            // background
-            // backgroundPadding={6}
-            styles={buildStyles({
-              pathTransitionDuration: 0.5,
-              strokeLinecap: "butt",
-              rotation: 1 / 2 + 1 / 8,
-              pathColor: `rgba(${(63 / percentage) * 100}, ${
-                percentage < 15 ? 160 : 199
-              }, 44)`,
-              textColor: `rgba(${(63 / percentage) * 100}, ${
-                percentage < 15 ? 160 : 199
-              }, 44)`,
-              trailColor: "lightgray",
-              fontSize: "10px",
-              //   backgroundColor: "#061A94",
-            })}
-          />
-          {/* <span className={styles.workTimer}>20:00</span> */}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div className={styles.app}>
+				<img
+					src="images/pomodoro2icon.png"
+					alt="pomodoro"
+					className={styles.pomodoroImg}
+				/>
+				<div className={styles.controls}>
+					<AiOutlinePlayCircle color="black" size="3em" cursor="pointer" />
+					<AiOutlinePauseCircle color="black" size="3em" cursor="pointer" />
+					<RxReset color="black" size="3em" cursor="pointer" />
+					<BsSkipForward color="black" size="3em" cursor="pointer" />
+				</div>
+				<div className={styles.timer}>
+					<CircularProgressbar
+						value={percentage}
+						text={timeInMinutes ? `${timePassed}` : `${percentage}%`}
+						strokeWidth={6}
+						circleRatio={0.75}
+						className={styles.progress}
+						// background
+						// backgroundPadding={6}
+						styles={buildStyles({
+							pathTransitionDuration: 0.5,
+							strokeLinecap: "butt",
+							rotation: 1 / 2 + 1 / 8,
+							pathColor: `rgba(${(63 / percentage) * 100}, ${
+								percentage < 15 ? 160 : 199
+							}, 44)`,
+							textColor: `rgba(${(63 / percentage) * 100}, ${
+								percentage < 15 ? 160 : 199
+							}, 44)`,
+							trailColor: "lightgray",
+							fontSize: "10px",
+							//   backgroundColor: "#061A94",
+						})}
+					/>
+					{/* <span className={styles.workTimer}>20:00</span> */}
+				</div>
+			</div>
+		</div>
+	);
 }
 export default PomodoroTimer;
