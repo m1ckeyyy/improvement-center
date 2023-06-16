5642be1a817021c9572e76f236bc80a1ea6259c5
m1ckeyyy <90149242+m1ckeyyy@users.noreply.github.com>
2023-06-16 09:29:04 +0000

    commit before implementing a new timer logic for pomodoroTimer

--- client/src/pages/Tools/PomodoroTimer/PomodoroFunctions.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroFunctions.jsx
@@ -1,63 +1,59 @@
 import { useState, useEffect } from "react";
 
 export function usePomodoroTimer() {
-	const [timePassed, setTimePassed] = useState(
-		JSON.parse(localStorage.getItem("timePassed")) || 0
-	);
-	const [workMode, setWorkMode] = useState(
-		JSON.parse(localStorage.getItem("workMode")) || true
-	);
-	const [workTime, setWorkTime] = useState(
-		JSON.parse(localStorage.getItem("workTime")) || 25
-	);
-	const [breakTime, setBreakTime] = useState(
-		JSON.parse(localStorage.getItem("breakTime")) || 5
-	);
-	const [theme, setTheme] = useState(
-		JSON.parse(localStorage.getItem("theme")) || "default"
-	);
-	const [music, setMusic] = useState(
-		JSON.parse(localStorage.getItem("music")) || "on"
-	);
+  const [timePassed, setTimePassed] = useState(
+    JSON.parse(localStorage.getItem("timePassed")) || 0
+  );
+  const [workMode, setWorkMode] = useState(
+    JSON.parse(localStorage.getItem("workMode")) || true
+  );
+  const [workTime, setWorkTime] = useState(
+    JSON.parse(localStorage.getItem("workTime")) || 25
+  );
+  const [breakTime, setBreakTime] = useState(
+    JSON.parse(localStorage.getItem("breakTime")) || 5
+  );
+  const [theme, setTheme] = useState(
+    JSON.parse(localStorage.getItem("theme")) || "default"
+  );
+  const [music, setMusic] = useState(
+    JSON.parse(localStorage.getItem("music")) || "on"
+  );
 
-	
+  const resetAllSettings = () => {
+    localStorage.removeItem("timerState");
+  };
 
-	const clearTimerState = () => {
-		localStorage.removeItem("timerState");
-	};
+  const handlePageRefresh = () => {
+    localStorage.setItem("startTime", JSON.stringify(startTime));
+    localStorage.setItem("timePassed", JSON.stringify(timePassed));
+    localStorage.setItem("workMode", JSON.stringify(workMode));
+    localStorage.setItem("workTime", JSON.stringify(workTime));
+    localStorage.setItem("breakTime", JSON.stringify(breakTime));
+    localStorage.setItem("theme", JSON.stringify(theme));
+    localStorage.setItem("music", JSON.stringify(music));
+  };
 
-	const handlePageRefresh = () => {
-		const timerState = {
-			timePassed,
-			workMode,
-			workTime,
-			breakTime,
-			theme,
-			music,
-		};
-		localStorage.setItem("timerState", JSON.stringify(timerState));
-	};
+  useEffect(() => {
+    window.addEventListener("beforeunload", handlePageRefresh);
 
-	useEffect(() => {
-		window.addEventListener("beforeunload", handlePageRefresh);
+    return () => {
+      window.removeEventListener("beforeunload", handlePageRefresh);
+    };
+  }, [timePassed, workMode, workTime, breakTime, theme, music]);
 
-		return () => {
-			window.removeEventListener("beforeunload", handlePageRefresh);
-		};
-	}, [timePassed, workMode, workTime, breakTime, theme, music]);
-
-	return {
-		timePassed,
-		setTimePassed,
-		workMode,
-		setWorkMode,
-		workTime,
-		setWorkTime,
-		breakTime,
-		setBreakTime,
-		theme,
-		setTheme,
-		music,
-		setMusic,
-	};
+  return {
+    timePassed,
+    setTimePassed,
+    workMode,
+    setWorkMode,
+    workTime,
+    setWorkTime,
+    breakTime,
+    setBreakTime,
+    theme,
+    setTheme,
+    music,
+    setMusic,
+  };
 }
