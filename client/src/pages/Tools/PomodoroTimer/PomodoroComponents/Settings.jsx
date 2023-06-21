aff1d02f9734966a578107ff1664559316501a49
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2023-06-21 23:08:44 +0200

    minor changes to settings

--- client/src/pages/Tools/PomodoroTimer/PomodoroComponents/Settings.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroComponents/Settings.jsx
@@ -15,17 +15,20 @@ const Settings = ({
 	return (
 		<div className={styles.container}>
 			<div className={styles.settingsContainer}>
-				<h2>FORMA TYTUŁ</h2>
+				<h2>SETTINGS</h2>
 				<form onSubmit={applySettings}>
 					<h4>Set Time Preferences (Minutes)</h4>
 					<div className={styles.workTimePreference}>
 						<span>Work time: </span>
-						<input type="number" value={workTime} min="5" max="90" />
+						<input type="number" defaultValue={workTime} min="5" max="90" />
 					</div>
 					<div className={styles.breakTimePreference}>
 						<span>Break time: </span>
-						<input type="number" value={breakTime} min="5" max="90" />
+						<input type="number" defaultValue={breakTime} min="5" max="90" />
 					</div>
+					<button type="submit" className={styles.submitBtn}>
+						Apply
+					</button>
 				</form>
 				<button onClick={toggleVisibility} className={styles.closeBtn}>
 					<BiExit />
