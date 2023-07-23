214cffc2422fe7f7f57cdcecddea0fd2e4e08ecf
m1ckeyyy <90149242+m1ckeyyy@users.noreply.github.com>
2023-07-23 13:30:06 +0000

    matched controls container resolution with settings container in pomodoro + added @mediaquery styling to controls, todo next: settings container restyling

--- client/src/pages/Tools/PomodoroTimer/PomodoroComponents/Controls.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroComponents/Controls.jsx
@@ -6,25 +6,27 @@ import { MdAlarmOff } from 'react-icons/md';
 export function Controls({ isRunning, handleStartStop, handleReset, handleSkipSection }) {
   return (
     <div className={styles.controls}>
-      {isRunning ? (
-        <span onClick={handleStartStop}>
-          <AiOutlinePauseCircle color="#0a789b" size="3em" cursor="pointer" title="pause" />
-          <p>Pause</p>
+      <div className={styles.controlsItems}>
+        {isRunning ? (
+          <span onClick={handleStartStop}>
+            <AiOutlinePauseCircle color="#0a789b" size="3em" cursor="pointer" title="pause" />
+            <p>Pause</p>
+          </span>
+        ) : (
+          <span onClick={handleStartStop}>
+            <AiOutlinePlayCircle color="#0a789b" size="3em" cursor="pointer" title="play" />
+            <p>Play</p>
+          </span>
+        )}
+        <span onClick={handleReset}>
+          <MdAlarmOff color="#0a789b" size="3em" cursor="pointer" title="reset" />
+          <p>Reset</p>
         </span>
-      ) : (
-        <span onClick={handleStartStop}>
-          <AiOutlinePlayCircle color="#0a789b" size="3em" cursor="pointer" title="play" />
-          <p>Play</p>
+        <span onClick={handleSkipSection}>
+          <BsSkipForward color="#0a789b" size="3em" cursor="pointer" title="skip section" />
+          <p>Skip</p>
         </span>
-      )}
-      <span onClick={handleReset}>
-        <MdAlarmOff color="#0a789b" size="3em" cursor="pointer" title="reset" />
-        <p>Reset</p>
-      </span>
-      <span onClick={handleSkipSection}>
-        <BsSkipForward color="#0a789b" size="3em" cursor="pointer" title="skip section" />
-        <p>Skip</p>
-      </span>
+      </div>
     </div>
   );
 }
