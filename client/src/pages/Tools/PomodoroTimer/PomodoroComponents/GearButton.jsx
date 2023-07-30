7fb634eb8ab2c3d0880147b25d6ebd8391028f12
m1ckeyy <mikolaj.krawczynski3@gmail.com>
2023-07-30 21:36:17 +0200

    minor changes

--- client/src/pages/Tools/PomodoroTimer/PomodoroComponents/GearButton.jsx
+++ client/src/pages/Tools/PomodoroTimer/PomodoroComponents/GearButton.jsx
@@ -1,9 +1,9 @@
 import { BsFillGearFill } from 'react-icons/bs';
 import styles from './../PomodoroTimer.module.scss';
 
-export const GearButton = () => {
+export const GearButton = ({ onClick }) => {
   return (
-    <button className={styles.gearButton}>
+    <button className={styles.gearButton} onClick={onClick}>
       <BsFillGearFill size="35" color="#0a789b" title="settings" />
     </button>
   );
