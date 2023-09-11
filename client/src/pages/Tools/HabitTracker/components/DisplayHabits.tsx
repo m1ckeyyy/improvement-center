cc40ed755e303a2fac317110eca04fc7b888735c
m1ckeyyy <90149242+m1ckeyyy@users.noreply.github.com>
2023-09-11 11:14:28 +0000

    added a custom hook useFormattedHabits.tsx

--- client/src/pages/Tools/HabitTracker/components/DisplayHabits.tsx
+++ client/src/pages/Tools/HabitTracker/components/DisplayHabits.tsx
@@ -1,25 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import styles from './../styles/HabitTracker.module.scss';
 import { useHabitTrackerContext } from './HabitContext';
 import { MdOutlineLibraryAdd } from 'react-icons/md';
-type TDay = [string, boolean];
+import { useFormattedHabits } from './../hooks/useFormattedHabits';
 
 export const DisplayHabits = () => {
   const { toggleHabitPanelVisibility, habits } = useHabitTrackerContext()!;
-
+  const { formattedHabits } = useFormattedHabits(habits);
+  console.log('f: ', formattedHabits);
   // const test: { [index: string]: string } = { aa: 'yeah' };
 
   //get current day of the week
   return (
     <div className={styles.displayHabits}>
       <h3>Current Date (13th September 2023)</h3>
-      {habits.map((habit, index) => {
-        const noDaysSelected = Object.values(habit.daysOfWeek).every((e) => e === false);
-        if (noDaysSelected) {
-          for (const key of Object.keys(habit.daysOfWeek)) {
-            habit.daysOfWeek[key as keyof typeof habit.daysOfWeek] = true;
-          }
-        }
+      {formattedHabits.map((habit, index) => {
         return (
           <div key={index} className={styles.habit}>
             <h3>{habit.name}</h3>
@@ -27,7 +22,8 @@ export const DisplayHabits = () => {
             <p>Completed: {habit.completed ? 'Yes' : 'No'}</p>
             <>
               Days:
-              {Object.entries(habit.daysOfWeek)
+              {/* formattedHabit.daysOfWeek.map((e)=><h1>{e}</h1>) */}
+              {/* {Object.entries(habit.daysOfWeek)
                 .filter(([day, isCompleted]: TDay) => isCompleted)
                 .map(([day, isCompleted]: TDay) => {
                   return (
@@ -35,7 +31,7 @@ export const DisplayHabits = () => {
                       {day}
                     </span>
                   );
-                })}
+                })} */}
             </>
           </div>
         );
---
+++ client/src/pages/Tools/HabitTracker/hooks/useFormattedHabits.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+import { HabitDataType } from './../components/useHabitData';
+
+type TDay = [string, boolean];
+
+export function useFormattedHabits(habits: HabitDataType[]) {
+  const [formattedHabits, setFormattedHabits] = useState<HabitDataType[]>([]);
+
+  useEffect(() => {
+    const updatedHabits = habits.map((habit: HabitDataType) => {
+      const noDaysSelected = Object.values(habit.daysOfWeek).every((e) => e === false);
+
+      if (noDaysSelected) {
+        for (const key of Object.keys(habit.daysOfWeek)) {
+          habit.daysOfWeek[key as keyof typeof habit.daysOfWeek] = true;
+        }
+      }
+      // Remove daysOfWeek that are set to false, and modify the array
+      // Object.entries(habit.daysOfWeek).filter(([day, isCompleted]: TDay) => isCompleted);
+
+      return habit;
+    });
+
+    setFormattedHabits(updatedHabits);
+  }, [habits]);
+
+  return { formattedHabits };
+}
